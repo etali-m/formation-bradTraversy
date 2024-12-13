73a770afee8bb9dc7f88b7f4105c169e2d4f2d19
etali-m <etalijunior15@gmail.com>
2024-12-13 07:26:06 +0100

    pagination for listing

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from . models import Listing
 
 # Create your views here.
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 3) #6 articles par page
+
+    #obtenir le numéro de la page dépuis les paramètre GET
+    page = request.GET.get('page')
+    paged_listing = paginator.get_page(page) #obtenir la page demandée
+
+    context = {
+        'listings' : paged_listing
+    }
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 
