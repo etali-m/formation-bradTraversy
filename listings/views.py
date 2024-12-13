903c9a2543432c6b9a718b04426cbcc00053a9a5
etali-m <etalijunior15@gmail.com>
2024-12-13 14:48:31 +0100

    Home & About page dymanic content

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from . models import Listing
+from .choices import state_choices, price_choices, bedroom_choices
 
 # Create your views here.
 def index(request):
@@ -12,14 +13,26 @@ def index(request):
     paged_listing = paginator.get_page(page) #obtenir la page demandée
 
     context = {
-        'listings' : paged_listing
+        'listings' : paged_listing,
     }
     return render(request, 'listings/listings.html', context)
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    #recupérer l'objet avec l'ID correspondant
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/searcht.html')
+
+    context = {
+        'state_choices': state_choices,
+        'price_choices':  price_choices,
+        'bedroom_choices': bedroom_choices
+    }
+    return render(request, 'listings/search.html', context)
